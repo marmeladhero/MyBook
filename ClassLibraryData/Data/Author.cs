9513ce1427bcec95361dd126ae892a74f11b9a9a
marmeladhero <marmeladhero@gmail.com>
2018-07-18 19:12:02 +0300

    Убрал не нужное

--- ClassLibraryData/Data/Author.cs
+++ ClassLibraryData/Data/Author.cs
@@ -10,7 +10,6 @@ namespace ClassLibraryData.Data
 {
     public class Author
     {
-        [HiddenInput(DisplayValue =false)]
         [Display(Name="ID")]
         public int Id { get; set; }
 
@@ -22,22 +21,5 @@ namespace ClassLibraryData.Data
 
         [Display(Name = "Second name")]
         public string SecondName { get; set; }
-
-        public static string CreateMD5(string input)
-        {
-          
-            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-                
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    sb.Append(hashBytes[i].ToString("X2"));
-                }
-                return sb.ToString();
-            }
-        }
     }
 }
